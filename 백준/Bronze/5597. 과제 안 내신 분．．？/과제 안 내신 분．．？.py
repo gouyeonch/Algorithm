4261c7a240684d6624b37583538364431427d425
Gouyeon Chung <gouyeonch@naver.com>
2023-06-26 11:24:01 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -4,8 +4,6 @@ list = list(range(1, 31))
 
 for i in range(28):
     ind = int(sys.stdin.readline())
-    list[ind-1] = 0
+    list.remove(ind)
 
-new_list = [i for i in list if i != 0]
-
-print(' '.join(map(str, new_list)))
+print(' '.join(map(str, list)))
