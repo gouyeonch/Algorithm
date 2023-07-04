35c8722a63a42966d21a5fa8ed4815ea35998903
Gouyeon Chung <gouyeonch@naver.com>
2023-07-04 10:38:38 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -5,18 +5,8 @@ resStr = ""
 n = int(sys.stdin.readline())
 
 for i in range(n):
-    q = 0
-    d = 0
-    n = 0
-    p = 0
     cent = int(sys.stdin.readline())
-    q = cent // 25
-    cent %= 25
-    d = cent // 10
-    cent %= 10
-    n = cent // 5
-    cent %= 5
-    p = cent
-
-    print(str(q) + " " + str(d) + " " + str(n) + " " + str(p))
+    for j in [25, 10, 5, 1]:
+        print(cent//j, end=' ')
+        cent %= j
 
