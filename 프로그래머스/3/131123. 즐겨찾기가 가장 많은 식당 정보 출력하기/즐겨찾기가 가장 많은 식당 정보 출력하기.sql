ed7d9d1640102c52842089f1e4c379d617f50efc
Gouyeon Chung <gouyeonch@naver.com>
2024-02-09 17:25:06 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+select FOOD_TYPE,	REST_ID,	REST_NAME,	FAVORITES
+from rest_info
+where (FOOD_TYPE, FAVORITES) in (
+    select FOOD_TYPE, max(FAVORITES) as FAVORITES
+    from rest_info
+    group by FOOD_TYPE
+)
+order by FOOD_TYPE desc
