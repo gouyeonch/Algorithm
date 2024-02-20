ebe7260890c25159449af955eeb3d77295f6bed2
Gouyeon Chung <gouyeonch@naver.com>
2024-02-20 18:15:40 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+select t.ITEM_ID, i.ITEM_NAME
+    from ITEM_TREE t
+        join ITEM_INFO i
+        on t.ITEM_ID = i.ITEM_ID
+    where PARENT_ITEM_ID is NULL
+    order by t.ITEM_ID
