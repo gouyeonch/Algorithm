21d5e25f167ed69fc1933643fd147e5d75453275
Gouyeon Chung <gouyeonch@naver.com>
2024-02-27 04:39:02 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,19 +1,8 @@
 -- 코드를 작성해주세요
-# select *
-#     from SKILLCODES 
 select ID, EMAIL, FIRST_NAME, LAST_NAME
     from DEVELOPERS
     where
-    (
-        (length(bin(SKILL_CODE)) >= (select length(bin(CODE)) from SKILLCODES where NAME = 'Python'))
-        and
-        (left(right(cast(bin(SKILL_CODE) as char),(select length(bin(CODE)) from SKILLCODES where NAME = 'Python')),1)) = 1
-    )
-    or
-    (
-        (length(bin(SKILL_CODE)) >= (select length(bin(CODE)) from SKILLCODES where NAME = 'C#'))
-        and
-        (left(right(cast(bin(SKILL_CODE) as char),(select length(bin(CODE)) from SKILLCODES where NAME = 'C#')),1)) = 1
-    )
+        (SKILL_CODE & (select CODE from SKILLCODES where NAME = 'C#')) > 1
+        or
+        (SKILL_CODE & (select CODE from SKILLCODES where NAME = 'Python')) > 1
     order by id
-
